d62228a59736ad6ce8f71c3cbe2eab9a0cc84073
Gonzalo Martinez <gon.martinez@icloud.com>
2020-08-28 11:57:33 -0300

    Movi el boton de nuevo

--- EjemploGit2/Form1.Designer.vb
+++ EjemploGit2/Form1.Designer.vb
@@ -27,7 +27,7 @@ Partial Class Form1
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(62, 64)
+        Me.Button1.Location = New System.Drawing.Point(48, 44)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 0
